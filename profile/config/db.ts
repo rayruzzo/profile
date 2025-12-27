84b5422d2123c5f0349908b7c7cb516ce2d2a49e
Ray Ruzzo <rrayruzz@gmail.com>
2025-12-26 19:00:15 -0500

    updating db connection string

--- profile/config/db.ts
+++ profile/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/profile';
+const MONGODB_URI = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017/profile';
 
 export default async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
